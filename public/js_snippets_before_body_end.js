afc4a08244a6a0aa34b0cd1e8fa1f8456b0f2098
Kai-Uwe Humpert <kai-uwe.humpert@justrelate.com>
2023-03-27 14:46:34 +0200

    R.I.P. js_snippets

--- public/js_snippets_before_body_end.js
+++
@@ -1,2 +0,0 @@
-// This file is for executing JavaScript code immediately before the body element is closed.
-// Here you can, for example, let third-party code render additional markup.
--- src/prerenderContent/generateHtml.js
+++ src/prerenderContent/generateHtml.js
@@ -21,14 +21,11 @@ export async function generateHtml({
     <link rel="preconnect" href="https://api.scrivito.com" />
     <link rel="preconnect" href="https://cdn0.scrvt.com" />
     <link rel="stylesheet" href="${assetManifest["index.css"]}" />
-
-    <script src="/js_snippets_head.js"></script>
   </head>
   <body ${bodyAttributes}>
     <div id="application" data-scrivito-prerendering-obj-id="${objId}">${bodyContent}</div>
     <script src="${preloadDumpFileName}"></script>
     <script async src="${assetManifest["index.js"]}"></script>
-    <script src="/js_snippets_before_body_end.js"></script>
   </body>
 </html>
 `;
