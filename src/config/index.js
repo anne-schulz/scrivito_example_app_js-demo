19211a9619df16d101e2dee7123d5406c0e70b0b
Kai-Uwe Humpert <kai-uwe.humpert@justrelate.com>
2023-06-16 00:26:55 +0200

    [i18] Deutsche Zeitangaben im Blog

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,8 @@ import { configureHistory } from "./history";
 import { configureObjClassForContentType } from "./objClassForContentType";
 import { configureScrivitoContentBrowser } from "./scrivitoContentBrowser";
 import { configureWindowScrivito } from "./windowScrivito";
+import { i18n } from "dateformat";
+
 
 export function configure(options) {
   configureScrivito(options);
@@ -12,3 +14,47 @@ export function configure(options) {
   configureHistory();
   configureWindowScrivito();
 }
+
+i18n.dayNames = [
+  "So",
+  "Mo",
+  "Di",
+  "Mi",
+  "Do",
+  "Fr",
+  "Sa",
+  "Sonntag",
+  "Montag",
+  "Dienstag",
+  "Mittwoch",
+  "Donnerstag",
+  "Freitag",
+  "Samstag",
+];
+
+i18n.monthNames = [
+  "Jan",
+  "Feb",
+  "Mär",
+  "Apr",
+  "Mai",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Okt",
+  "Nov",
+  "Dez",
+  "Januar",
+  "Februar",
+  "März",
+  "April",
+  "Mai",
+  "Juni",
+  "Juli",
+  "August",
+  "September",
+  "Oktober",
+  "November",
+  "Dezember",
+];
